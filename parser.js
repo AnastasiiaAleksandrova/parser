31e5852fb83ea0b0156016c51763df0a0738ce0f
AnastasiiaAleksandrova <47037922+AnastasiiaAleksandrova@users.noreply.github.com>
2019-11-14 15:54:04 +0200

    done but bugs with colon parsing

--- parser.js
+++ parser.js
@@ -12,13 +12,24 @@ packages.forEach(package => {
     
     let packageObj = chunks.reduce((acc, cur, idx, src) => {
         if (cur == 'Package' || cur == 'Description') {
-            acc[cur.toLowerCase()] = src[idx + 1];  
+            acc[cur.toLowerCase()] = src[idx + 1];
+            
         } else if (cur == 'Depends') {
-            acc[cur.toLowerCase()] = src[idx + 1].replace(/\s\((.*?)\)/g, '');
+            let depends = new Set(
+                src[idx + 1]
+                .replace(/\s\((.*?)\)/g, '')
+                .split(/,\s/)
+            );
+            depends.forEach(el => {
+                if (!finalList.has(el)) {
+                    finalList.set(el, {reversDep: acc['package']})
+                }
+            });
+            acc[cur.toLowerCase()] = depends;   
         }
         return acc;
-    }, {}) 
-    finalList.set(packageObj.package, packageObj);  
+    }, {});
+    finalList.set(packageObj.package, {...packageObj});  
 })
 
 console.dir(finalList);
