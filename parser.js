196c31990db33e55e0cc58470895a6b0a524d755
AnastasiiaAleksandrova <47037922+AnastasiiaAleksandrova@users.noreply.github.com>
2019-11-14 14:40:06 +0200

    parsing without reverse depends

---
+++ parser.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+let text = fs.readFileSync('raw.txt').toString();
+let packages = text.split(/\r\n\r\n|\r\r|\n\n/);
+let finalList = new Map();
+
+packages.forEach(package => {
+    let chunks = package
+    .replace(/\r\n\s/g, ' ')
+    .replace(/:\s/g, ':')
+    .split(/\r\n|:/);
+    
+    let packageObj = chunks.reduce((acc, cur, idx, src) => {
+        if (cur == 'Package' || cur == 'Description') {
+            acc[cur.toLowerCase()] = src[idx + 1];  
+        } else if (cur == 'Depends') {
+            acc[cur.toLowerCase()] = src[idx + 1].replace(/\s\((.*?)\)/g, '');
+        }
+        return acc;
+    }, {}) 
+    finalList.set(packageObj.package, packageObj);  
+})
+
+console.dir(finalList);
+
+
+
+
+
